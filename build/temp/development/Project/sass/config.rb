f469bd2acb49d1071e90a5dfa5a5351667a08047
komar <komar.udin@reebonz.com>
2015-09-14 10:36:06 +0700

    delete all

--- build/temp/development/Project/sass/config.rb
+++
@@ -1,2 +0,0 @@
-Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
-cache_path = 'c:\xampp\htdocs\project/build/.sass-cache'
--- build/temp/development/Project/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require c:\xampp\htdocs\project\app.js
